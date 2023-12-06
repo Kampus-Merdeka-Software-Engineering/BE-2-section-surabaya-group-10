fab47c02fab2e41ed5a5e680f88e83149372fc93
Kavatsmara Fayra <126572167+gavrielakav@users.noreply.github.com>
2023-12-07 00:27:47 +0700

    Update code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { PrismaClient } = require("@prisma/client");
 
 const app = express();
-const port = 5000;
+const port = 5555;
 const prisma = new PrismaClient();
 
 app.use(cors());
--- src/routes/booking.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-    const bookings = require("../controllers/booking.controller.js");
-    const router = require("express").Router();
-
-    router.post("/", bookings.createBooking);
-
-    app.use("/api/bookings", router);
-}
