ed3243fb676c3932667f2785b5b6556e113f93a7
Kavatsmara Fayra <126572167+gavrielakav@users.noreply.github.com>
2023-12-09 00:22:16 +0700

    delete

--- src/routes/email.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-    const email = require("../controllers/email.controller.js");
-    const router = require("express").Router();
-
-    router.post("/", email.create);
-
-    app.use("/api/email", router);
-}
